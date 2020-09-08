a52b3a5eecc671dbeead27351b62bab870924835
Josh Maximoff <jmaximoff@numurus.com>
2020-09-08 18:18:45 -0400

    Separate API sections with comment blocks

--- include/nepi_edge_lb_interface.h
+++ include/nepi_edge_lb_interface.h
@@ -7,10 +7,12 @@
 #include "nepi_edge_lb_consts.h"
 #include "nepi_edge_errors.h"
 
+/* **************** Status and Data Message API **************** */
 typedef void* NEPI_EDGE_LB_Status_t;
 NEPI_EDGE_RET_t NEPI_EDGE_LBStatusCreate(NEPI_EDGE_LB_Status_t *status, const char* timestamp_rfc3339);
 NEPI_EDGE_RET_t NEPI_EDGE_LBStatusDestroy(NEPI_EDGE_LB_Status_t status);
-NEPI_EDGE_RET_t NEPI_EDGE_LBStatusSetNavSatFixTime(NEPI_EDGE_LB_Status_t status, const char* timestamp_iso8601);
+
+NEPI_EDGE_RET_t NEPI_EDGE_LBStatusSetNavSatFixTime(NEPI_EDGE_LB_Status_t status, const char* timestamp_rfc3339);
 NEPI_EDGE_RET_t NEPI_EDGE_LBStatusSetLatitude(NEPI_EDGE_LB_Status_t status, float latitude_deg);
 NEPI_EDGE_RET_t NEPI_EDGE_LBStatusSetLongitude(NEPI_EDGE_LB_Status_t status, float longitude_deg);
 NEPI_EDGE_RET_t NEPI_EDGE_LBStatusSetHeading(NEPI_EDGE_LB_Status_t status, NEPI_EDGE_Heading_Ref_t heading_ref, float heading_deg);
@@ -23,7 +25,8 @@ NEPI_EDGE_RET_t NEPI_EDGE_LBStatusSetDeviceStatus(NEPI_EDGE_LB_Status_t status, 
 typedef void* NEPI_EDGE_LB_Data_Snippet_t;
 NEPI_EDGE_RET_t NEPI_EDGE_LBDataSnippetCreate(NEPI_EDGE_LB_Data_Snippet_t *snippet, const char type[NEPI_EDGE_DATA_SNIPPET_TYPE_LENGTH], uint32_t instance);
 NEPI_EDGE_RET_t NEPI_EDGE_LBDataSnippetDestroy(NEPI_EDGE_LB_Data_Snippet_t snippet);
-NEPI_EDGE_RET_t NEPI_EDGE_LBDataSnippetSetDataTimestamp(NEPI_EDGE_LB_Data_Snippet_t snippet, const char* data_time_iso8601);
+
+NEPI_EDGE_RET_t NEPI_EDGE_LBDataSnippetSetDataTimestamp(NEPI_EDGE_LB_Data_Snippet_t snippet, const char* timestamp_rfc3339);
 NEPI_EDGE_RET_t NEPI_EDGE_LBDataSnippetSetLatitude(NEPI_EDGE_LB_Data_Snippet_t snippet, float latitude_deg);
 NEPI_EDGE_RET_t NEPI_EDGE_LBDataSnippetSetLongitude(NEPI_EDGE_LB_Data_Snippet_t snippet, float longitude_deg);
 NEPI_EDGE_RET_t NEPI_EDGE_LBDataSnippetSetHeading(NEPI_EDGE_LB_Data_Snippet_t snippet, float heading_deg);
@@ -34,15 +37,19 @@ NEPI_EDGE_RET_t NEPI_EDGE_LBDataSnippetSetDataFile(NEPI_EDGE_LB_Data_Snippet_t s
 
 NEPI_EDGE_RET_t NEPI_EDGE_LBExportData(const NEPI_EDGE_LB_Status_t status, const NEPI_EDGE_LB_Data_Snippet_t *snippets, size_t snippet_count);
 
+/* **************** Config Message API **************** */
 typedef void* NEPI_EDGE_LB_Config_t;
 NEPI_EDGE_RET_t NEPI_EDGE_LBConfigCreate(NEPI_EDGE_LB_Config_t *config);
 NEPI_EDGE_RET_t NEPI_EDGE_LBConfigDestroy(NEPI_EDGE_LB_Config_t config);
 
 NEPI_EDGE_RET_t NEPI_EDGE_LBImportConfig(NEPI_EDGE_LB_Config_t config, const char* filename);
+// TODO: Field extraction methods
 
+/* **************** General Message API **************** */
 typedef void* NEPI_EDGE_LB_General_t;
 NEPI_EDGE_RET_t NEPI_EDGE_LBGeneralCreate(NEPI_EDGE_LB_General_t *general);
 NEPI_EDGE_RET_t NEPI_EDGE_LBGeneralDestroy(NEPI_EDGE_LB_General_t general);
+
 NEPI_EDGE_RET_t NEPI_EDGE_LBGeneralSetPayloadStrBool(NEPI_EDGE_LB_General_t general, const char *id, uint8_t val);
 NEPI_EDGE_RET_t NEPI_EDGE_LBGeneralSetPayloadStrInt64(NEPI_EDGE_LB_General_t general, const char *id, int64_t val);
 NEPI_EDGE_RET_t NEPI_EDGE_LBGeneralSetPayloadStrUInt64(NEPI_EDGE_LB_General_t general, const char *id, uint64_t val);
@@ -59,6 +66,8 @@ NEPI_EDGE_RET_t NEPI_EDGE_LBGeneralSetPayloadIntStr(NEPI_EDGE_LB_General_t gener
 NEPI_EDGE_RET_t NEPI_EDGE_LBGeneralSetPayloadIntBytes(NEPI_EDGE_LB_General_t general, uint32_t id, const uint8_t *val, const size_t length);
 
 NEPI_EDGE_RET_t NEPI_EDGE_LBExportGeneral(NEPI_EDGE_LB_General_t general);
+
 NEPI_EDGE_RET_t NEPI_EDGE_LBImportGeneral(NEPI_EDGE_LB_General_t general, const char* filename);
+// TODO Field extraction methods
 
 #endif //__NEPI_EDGE_LB_INTERFACE_H

37e128fc0df14702978e77f915d3da433a85d60e
Josh Maximoff <jmaximoff@numurus.com>
2020-09-08 11:52:50 -0400

    Some C SDK example updates
    1. Update timestamps in C SDK example
    2. Use a more natural path for data snippet files

--- examples/c/nepi_sdk_example_session.c
+++ examples/c/nepi_sdk_example_session.c
@@ -5,12 +5,13 @@
 
 int main(int argc, char **argv)
 {
+  printf("Testing NEPIEdgeSdk (C-language bindings)\n");
   /* First, set the the NEPI-EDGE filesys interface path */
   NEPI_EDGE_SetBotBaseFilePath("./nepi_sdk_example_filesys");
 
   /* Now create the status */
   NEPI_EDGE_LB_Status_t status;
-  NEPI_EDGE_LBStatusCreate(&status, "2020-08-21 09:50:25.431396857-04:00"); // Timestamp is required; hence included in Create method
+  NEPI_EDGE_LBStatusCreate(&status, "2020-09-03 17:14:25.2-04:00"); // Timestamp is required; hence included in Create method
   /* Populate some of the optional fields */
   NEPI_EDGE_LBStatusSetNavSatFixTime(status, "2020-08-21T09:49:00.0-04:00"); // Either RFC3339 time/date separator is allowed.
   NEPI_EDGE_LBStatusSetLatitude(status, 47.6062f); // Seattle
@@ -25,20 +26,22 @@ int main(int argc, char **argv)
 
   /* Next create a couple of data snippets */
   NEPI_EDGE_LB_Data_Snippet_t data_snippets[2];
-  NEPI_EDGE_LBDataSnippetCreate(&data_snippets[0], "abc", 0); // Type and instance are required fields; hence in the Create method
+  NEPI_EDGE_LBDataSnippetCreate(&data_snippets[0], "cls", 0); // Type and instance are required fields; hence in the Create method
   /* And populate some of the optional fields */
-  NEPI_EDGE_LBDataSnippetSetDataTimestamp(data_snippets[0], "2020-08-21 09:50:25.6-04:00"); // 169ms after the status timestamp
+  NEPI_EDGE_LBDataSnippetSetDataTimestamp(data_snippets[0], "2020-09-03 17:14:25.6-04:00"); // 169ms after the status timestamp
   NEPI_EDGE_LBDataSnippetSetLatitude(data_snippets[0], 47.607f); // Small motion northward from position at status timestamp
   NEPI_EDGE_LBDataSnippetSetLongitude(data_snippets[0], -122.33f); // Small motion eastward from position at status timestamp
   NEPI_EDGE_LBDataSnippetSetHeading(data_snippets[0], 22.5f); // North-north east
   NEPI_EDGE_LBDataSnippetSetRollAngle(data_snippets[0], 6.5f); // Another degree of roll from attitude at status timestamp
   NEPI_EDGE_LBDataSnippetSetPitchAngle(data_snippets[0], -18.2f); // 0.6 degrees more downward pitch
   NEPI_EDGE_LBDataSnippetSetScores(data_snippets[0], 0.33f, 1.0f, 0.5f);
-  const char *simulated_file_path = "./sdk_example_data_1.bin"; // Will be copied into allocated memory, so this can be a temporary variable
+  const char *simulated_file_path = "left_img_detections.txt"; // Will be copied into allocated memory, so this can be a temporary variable
   NEPI_EDGE_LBDataSnippetSetDataFile(data_snippets[0], simulated_file_path);
 
-  /* Now the second data snippet -- empty except for the required type and instance fields */
-  NEPI_EDGE_LBDataSnippetCreate(&data_snippets[1], "def", 55);
+  /* Now the second data snippet -- empty except for the required type and instance fields and optional data file field */
+  NEPI_EDGE_LBDataSnippetCreate(&data_snippets[1], "cls", 1);
+  const char *simulated_file_path_2 = "left_img_small.jpeg"; // Will be copied into allocated memory, so this can be a temporary variable
+  NEPI_EDGE_LBDataSnippetSetDataFile(data_snippets[1], simulated_file_path_2);
 
   /* And export the status+data */
   NEPI_EDGE_LBExportData(status, data_snippets, 2);
@@ -58,7 +61,7 @@ int main(int argc, char **argv)
   /* Populate this one with a numerical key and an arbitrary byte array for its value */
   uint8_t byte_array[4] = {0xDE,0xAD,0xBE,0xEF};
   NEPI_EDGE_LBGeneralSetPayloadIntBytes(general_2, 12345, byte_array, 4);
-  /* And export it to a file (in the proper location) */
+  /* And export it to a file */
   NEPI_EDGE_LBExportGeneral(general_2);
 
   /* Now we demonstrate import */
